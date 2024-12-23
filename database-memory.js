e1ed502cc434cd35c766a26c6e8e3065fb782d5b
Ricardo Magalhães <ricardogleiss@hotmail.com>
2024-12-23 19:56:04 -0300

    Create in-memory database

--- database-memory.js
+++ database-memory.js
@@ -1,3 +1,46 @@
+import { randomUUID } from "node:crypto";
+
 export class DatabaseMemory {
-  #videos = [];
+  #videos = new Map();
+
+  list() {
+    
+    return Array.from(this.#videos.values());
+  }
+
+  find(id) {
+    return this.#videos.get(id);
+  }
+
+  create(video) {
+    const videoId = randomUUID();
+    
+    const newVideo = {
+      id: videoId,
+      ...video
+    }
+    this.#videos.set(videoId, newVideo)
+  }
+
+  update(id, info) {
+    const videoToUpdate = this.find(id);
+    // const videoToUpdate = this.#videos.get(id);
+    console.log(videoToUpdate);
+
+    if (!videoToUpdate) {
+      return "Video not found";
+    } 
+    const updatedVideo = {
+      ...videoToUpdate,
+      ...info
+    }
+
+    this.#videos.set(id, updatedVideo);
+
+    return updatedVideo;
+  }
+
+  delete(id) {
+    this.#videos.delete(id);
+  }
 }
